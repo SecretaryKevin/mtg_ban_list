e28d501c59b180062b5c064d2b5ec3984061b431
Daniel Templer <dtempler@outlook.co.nz>
2024-06-22 23:07:37 +1200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from "react";
 import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
-
+import {databaseQuerys} from "./utilities/databaseQuerys.js";
+import {queryDatabase} from "./utilities/databaseOperations.js";
 import {AuthContext} from "./Components/AuthContext.jsx";
 import {Home} from "./Pages/Home.jsx";
 import {Header} from "./Components/Header.jsx";
@@ -10,16 +11,18 @@ import {SuggestCard} from "./Pages/SuggestCard.jsx";
 import {AdminDashboard} from "./Pages/AdminDashboard.jsx";
 import {Unauthorised} from "./Pages/Unauthorised.jsx";
 
+
 function App() {
     const [user, setUser] = React.useState(null);
     const [BannedCards, setBannedCards] = React.useState([]);
     const [PendingCards, setPendingCards] = React.useState([]);
+    const [allUsers, setAllUsers] = React.useState([]);
 
     useEffect(() => {
-        //TODO: Fetch Banned cards and Pending cards from database
-        console.log('Fetching Banned Cards and Pending Cards');
-    }, []);
-
+        setBannedCards(queryDatabase(databaseQuerys.getAllBannedCards));
+        setPendingCards(queryDatabase(databaseQuerys.getPendingCards));
+        setAllUsers(queryDatabase(databaseQuerys.getUsers));
+    })
     function setUserInfo(userInfo) {
         console.log('User Info:', userInfo);
         setUser(userInfo);
